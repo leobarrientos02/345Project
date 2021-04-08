6d5bd8e65cf31d09f94abfb9ea97de0673451d65
agordon123 <59323177+agordon123@users.noreply.github.com>
2021-04-08 13:41:50 -0400

    scrapped test ideas to start clean
    deleted some sample layouts

--- src/semesterproject/ProjectLayoutController.java
+++ src/semesterproject/ProjectLayoutController.java
@@ -5,25 +5,13 @@
  */
 package semesterproject;
 
-import javafx.scene.Group;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
+
+
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.text.Text;
-import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class for Card Game Project
@@ -33,28 +21,6 @@ import javafx.scene.layout.Pane;
 public class ProjectLayoutController implements Initializable {
 
     
-    @FXML
-    private Pane midleftbox;
-    @FXML
-    private ImageView leftmidpic;
-    @FXML
-    private Pane midrightbox;
-    @FXML
-    private ImageView rightmidpic;
-    @FXML
-    private Pane leftbox;
-    @FXML
-    private ImageView leftpic;
-    @FXML
-    private Pane rightbox;
-    @FXML
-    private ImageView rightpic;
-    @FXML
-    private TextField toptextbox;
-    @FXML
-    private Button answerfinder;
-    @FXML
-    private Button refresh;
 
   
     @Override
