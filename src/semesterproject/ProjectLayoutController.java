01b46bf0bb47981333c0dd679a7b00a56ba903ee
agordon123 <59323177+agordon123@users.noreply.github.com>
2021-04-09 05:48:23 -0400

    started JAVAFX controller
    also added a playing card class to be filled in later

--- src/semesterproject/ProjectLayoutController.java
+++ src/semesterproject/ProjectLayoutController.java
@@ -12,6 +12,12 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class for Card Game Project
@@ -20,6 +26,31 @@ import javafx.fxml.Initializable;
  */
 public class ProjectLayoutController implements Initializable {
 
+    @FXML
+    private AnchorPane anchorpane;
+    @FXML
+    private ImageView Card1;
+    @FXML
+    private ImageView Card11;
+    @FXML
+    private ImageView Card111;
+    @FXML
+    private ImageView Card1111;
+    @FXML
+    private TextField answerDisplay;
+    @FXML
+    private Button cheatkey;
+    @FXML
+    private Button refresh;
+    @FXML
+    private TextArea TextExpression;
+    @FXML
+    private TextField checkAnswer;
+    @FXML
+    private Button verifyButton;
+    @FXML
+    private Button quit;
+
     
 
   
@@ -27,5 +58,22 @@ public class ProjectLayoutController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void displaySolution(ActionEvent event) {
+    }
+
+    @FXML
+    private void generateNewGame(ActionEvent event) {
+    }
+
+    @FXML
+    private void checkAnswer(ActionEvent event) {
+    }
+
+    @FXML
+    private void quitProgram(ActionEvent event) {
+    }
     
 }
+
