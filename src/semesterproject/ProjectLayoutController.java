40325c9dd815038a31f81024ded0735e27c3da47
Leonel Barrientos <leobarrientos02@gmail.com>
2021-04-11 23:40:09 -0400

    MajorUpdate

--- src/semesterproject/ProjectLayoutController.java
+++ src/semesterproject/ProjectLayoutController.java
@@ -29,6 +29,20 @@ import javafx.scene.layout.AnchorPane;
  */
 public class ProjectLayoutController implements Initializable {
 
+    
+    // Images
+    private InputStream stream;
+    private InputStream stream2;
+    private InputStream stream3;
+    private InputStream stream4;
+    
+    private Image image1;
+    private Image image2;
+    private Image image3;
+    private Image image4;
+    private String fileName;
+    private int value1, value2, value3, value4;
+    
     @FXML
     private AnchorPane anchorpane;
     @FXML
@@ -53,31 +67,208 @@ public class ProjectLayoutController implements Initializable {
     private Button verifyButton;
     @FXML
     private Button quit;
-  
+    
+            
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-
-    @FXML
-    private void displaySolution(ActionEvent event) {
+        try {
+            ShowRandomCard();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ProjectLayoutController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-
-    @FXML
-    private void generateNewGame(ActionEvent event) {
-        
-        refresh = (Button)event.getSource();
-        
-        switch( refresh.getText()){
-            
-            case "Refresh Cards":
     
-                Card1.setX(0);
-                Card1.setY(0);
-                
+    
+    public String Generate_RandomCard(){
+        //Generate a random card from the deck
+        fileName="";
+        String value ="";
+        String cardType = "";
+        //Generate Random number to choose from the array
+        Random rn = new Random();
+        int answer = rn.nextInt(4) + 1;
+        switch(answer){
+            case 1:
+                cardType = "spades";
+                break;
+            case 2:
+                cardType= "diamonds";
+                break;
+            case 3:
+                cardType="clubs";
+                break;
+            case 4:
+                cardType = "hearts";
+                break;
+            default:
+                break;
+        }
+    
+        // Generate Random number for card value
+        Random rn2 = new Random();
+        int answer2 = rn2.nextInt(12) + 1;
+        switch (answer2) {
+            case 1:
+                value = "ace";
+                break;
+            case 2:
+                value = "2";
+                break;
+            case 3:
+                value = "3";
+                break;
+            case 4:
+                value = "4";
+                break;
+            case 5:
+                value = "5";
+                break;
+            case 6:
+                value = "6";
+                break;
+            case 7:
+                value = "7";
+                break;
+            case 8:
+                value = "8";
+                break;
+            case 9:
+                value = "9";
+                break;
+            case 10:
+                value = "jack";
+                break;
+            case 11:
+                value = "queen";
+                break;
+            case 12:
+                value = "king";
+                break;
+            default:
                 break;
         }
         
+        return fileName = value + "_of_" + cardType + ".png";
+
+    }
+    
+    private int getCardValue(String fileName){
+        
+        int temp=0;
+        
+        char[] ch = new char[fileName.length()];
+        for(int i=0; i< fileName.length();i++){
+            ch[i] = fileName.charAt(i);
+        }
+        
+        switch (ch[0]) {
+            case 'a':
+                temp=1;
+                break;
+            case '2':
+                temp=2;
+                break;
+            case '3':
+                temp=3;
+                break;
+            case '4':
+                temp=4;
+                break;
+            case '5':
+                temp=5;
+                break;
+            case '6':
+                temp=6;
+                break;
+            case '7':
+                temp=7;
+                break;
+            case '8':
+                temp=8;
+                break;
+            case '9':
+                temp=9;
+                break;                
+            case 'j':
+                temp=10;
+                break;
+            case 'q':
+                temp=11;
+                break;
+            case 'k':
+                temp=12;
+                break;
+            default:
+                break;
+        }
+        
+        return temp;
+    }
+    
+    private void ShowRandomCard() throws FileNotFoundException{
+        String file = Generate_RandomCard();
+        stream = new FileInputStream("src/images/" + file);
+        image1 = new Image(stream);
+        
+        value1 = getCardValue(fileName);
+        
+        String file2 = Generate_RandomCard();
+        while(file.equals(file2)){
+            file2 = Generate_RandomCard();
+        }
+        if( !file.equals(file2)){
+            stream2 = new FileInputStream("src/images/"+ file2);
+            image2 = new Image(stream2);           
+        }
+        value2 = getCardValue(fileName);
+        
+        
+        String file3 = Generate_RandomCard();
+        while(file3.equals(file2) || file3.equals(file)){
+            file3 = Generate_RandomCard();
+        }
+        if(!file3.equals(file2) || !file3.equals(file)){
+            stream3 = new FileInputStream("src/images/" + file3);
+            image3 = new Image(stream3);            
+        }
+        value3 = getCardValue(fileName);     
+        
+        String file4 = Generate_RandomCard();
+        while(file4.equals(file3) || file4.equals(file3) || file4.equals(2)){
+            file4=Generate_RandomCard();
+        }
+        if(!file4.equals(file3) || !file4.equals(file3) || !file4.equals(2)){
+            stream4 = new FileInputStream("src/images/" + file4);
+            image4 = new Image(stream4);
+        }
+        value4 = getCardValue(fileName);
+        
+        int answer = value1 + value2 + value3 + value4;
+        answerDisplay.setText(String.valueOf(answer));
+        
+        
+        // Create the image view
+        Card1.setImage(image1);
+        Card11.setImage(image2);
+        Card111.setImage(image3);
+        Card1111.setImage(image4);
+    }
+
+    @FXML
+    private void displaySolution(ActionEvent event) {
+        cheatkey = (Button)event.getSource();
+        
+        switch( cheatkey.getText()){
+            case "Get Answer":
+
+                break;
+        }
+    }
+
+    @FXML
+    private void generateNewGame() throws FileNotFoundException{
+        
+            ShowRandomCard();   
     }
 
     @FXML
